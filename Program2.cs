95e3d2c68519d8ebbaaa7acf45ff2aba0a2d706b
Kurkudinov M. <neym18@inbox.ru>
2023-10-19 11:11:26 +0500

    Задание №2

---
+++ Program2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var people = new Dictionary<string, string>()
+            {
+                ["Bethoven"] = "Symphony",
+                ["Mozart"] = "Sonata",
+                ["Bach"] = "Concerto",
+                ["Chopin"] = "Opera",
+                ["Vivaldi"] = "Piano Concerto",
+                ["Haydn"] = "String Quartet",
+                ["Tchaikovsky"] = "Piano Concerto",
+                ["Handel"] = "Mass",
+                ["Brahms"] = "Requiem",
+                ["Verdi"] = "Suite"
+
+
+
+
+            };
+
+            var auth_key = Console.ReadLine();
+            var res = people.TryGetValue(auth_key, out var result);
+            if (res == true)
+            {
+                Console.WriteLine(people[auth_key]);
+                res = people.Remove(auth_key);
+                if (res == true) 
+                    Console.WriteLine("Успешно удалено");
+                foreach (var ppl in people)
+                    Console.WriteLine($"{ppl.Key}:{ppl.Value}");
+                people.Clear();
+                Console.WriteLine("Весь словарь очищен");
+
+            }
+            else
+            {
+                Console.WriteLine("Данный ключ не найден в словаре");
+            }
+
+            
+
+
+
+        }
+    }
+}
